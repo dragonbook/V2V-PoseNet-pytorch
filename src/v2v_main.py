501816b6ce1257c9a0b826e54bf88b6531531744
dragonbook <573260700@qq.com>
2018-12-02 19:08:29 +0800

    update

--- src/v2v_main.py
+++ src/v2v_main.py
@@ -49,7 +49,7 @@ def to_tensor(x):
     return torch.from_numpy(x)
 
 
-voxelization_train = V2VVoxelization(augmentation=False)
+voxelization_train = V2VVoxelization(augmentation=True)
 voxelization_val = V2VVoxelization(augmentation=False)
 
 
@@ -89,8 +89,8 @@ def transform_val(sample):
 # Datasets
 train_set = Tooth13Dataset(root=data_dir, mode='train', transform=transform_train)
 train_num = 1
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False, num_workers=6,sampler=ChunkSampler(train_num, 0))
-#train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=True, num_workers=6)
+#train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False, num_workers=6,sampler=ChunkSampler(train_num, 0))
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=True, num_workers=6)
 
 val_set = Tooth13Dataset(root=data_dir, mode='val', transform=transform_val)
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=1, shuffle=False, num_workers=6)
@@ -125,85 +125,85 @@ optimizer = optim.Adam(net.parameters())
 
 ## Train and validate
 print('Start train ..')
-for epoch in range(200):
+for epoch in range(50):
     print('Epoch: {}'.format(epoch))
     train_epoch(net, criterion, optimizer, train_loader, device=device, dtype=dtype)
     #val_epoch(net, criterion, val_loader, device=device, dtype=dtype)
 
 
 # Test
-# def test(model, test_loader, output_transform, device=torch.device('cuda'), dtype=torch.float):
-#     model.eval()
+def test(model, test_loader, output_transform, device=torch.device('cuda'), dtype=torch.float):
+    model.eval()
 
-#     samples_num = len(test_loader)
-#     keypoints = None
-#     idx = 0
+    samples_num = len(test_loader)
+    keypoints = None
+    idx = 0
 
-#     with torch.no_grad():
-#         for batch_idx, (inputs, refpoints) in enumerate(test_loader):
-#             outputs = model(inputs.to(device, dtype))
+    with torch.no_grad():
+        for batch_idx, (inputs, refpoints) in enumerate(test_loader):
+            outputs = model(inputs.to(device, dtype))
 
-#             outputs = outputs.cpu().numpy()
-#             refpoints = refpoints.cpu().numpy()
+            outputs = outputs.cpu().numpy()
+            refpoints = refpoints.cpu().numpy()
 
-#             # (batch, keypoints_num, 3)
-#             keypoints_batch = output_transform((outputs, refpoints))
+            # (batch, keypoints_num, 3)
+            keypoints_batch = output_transform((outputs, refpoints))
 
-#             if keypoints is None:
-#                 # Initialize keypoints until dimensions awailable now
-#                 keypoints = np.zeros((samples_num, *keypoints_batch.shape[1:]))
+            if keypoints is None:
+                # Initialize keypoints until dimensions awailable now
+                keypoints = np.zeros((samples_num, *keypoints_batch.shape[1:]))
 
-#             batch_size = keypoints_batch.shape[0]
-#             keypoints[idx:idx+batch_size] = keypoints_batch
-#             idx += batch_size
+            batch_size = keypoints_batch.shape[0]
+            keypoints[idx:idx+batch_size] = keypoints_batch
+            idx += batch_size
 
 
-#     return keypoints
+    return keypoints
    
 
-# def remove_dataset_scale(x):
-#     if isinstance(x, tuple):
-#         for e in x: e /= dataset_scale
-#     else: x /= dataset_scale
+def remove_dataset_scale(x):
+    if isinstance(x, tuple):
+        for e in x: e /= dataset_scale
+    else: x /= dataset_scale
 
-#     return x
-
-
-# voxelization_test = voxelization_train
-
-# def output_transform(x):
-#     heatmaps, refpoints = x
-#     keypoints = voxelization_test.evaluate(heatmaps, refpoints)
-#     return remove_dataset_scale(keypoints)
+    return x
 
 
-# def transform_test(sample):
-#     vertices, refpoint = sample['vertices'], sample['refpoint']
-#     vertices, refpoint = apply_dataset_scale((vertices, refpoint))
-#     input = voxelization_test.voxelize(vertices, refpoint)
-#     return to_tensor(input), to_tensor(refpoint.reshape((1, -1)))
+voxelization_test = voxelization_train
+
+def output_transform(x):
+    heatmaps, refpoints = x
+    keypoints = voxelization_test.evaluate(heatmaps, refpoints)
+    return remove_dataset_scale(keypoints)
 
 
-# test_set = Tooth13Dataset(root=data_dir, mode='test', transform=transform_test)
-# test_loader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=False, num_workers=6)
-
-# print('Start test ..')
-# keypoints_estimate = test(net, test_loader, output_transform, device, dtype)
-
-# test_res_filename = r'./test_res.txt'
-# print('Write result to ', test_res_filename)
-# # Reshape one sample keypoints in one line
-# result = keypoints_estimate.reshape(keypoints_estimate.shape[0], -1)
-# np.savetxt(test_res_filename, result, fmt='%0.4f')
+def transform_test(sample):
+    vertices, refpoint = sample['vertices'], sample['refpoint']
+    vertices, refpoint = apply_dataset_scale((vertices, refpoint))
+    input = voxelization_test.voxelize(vertices, refpoint)
+    return to_tensor(input), to_tensor(refpoint.reshape((1, -1)))
 
 
-# print('Start save fit ..')
-# fit_set = Tooth13Dataset(root=data_dir, mode='train', transform=transform_test)
-# fit_loader = torch.utils.data.DataLoader(fit_set, batch_size=1, shuffle=False, num_workers=6)
-# keypoints_fit = test(net, fit_loader, output_transform, device=device, dtype=dtype)
-# fit_res_filename = r'./fit_res.txt'
-# print('Write fit result to ', fit_res_filename)
-# fit_result = keypoints_fit.reshape(keypoints_fit.shape[0], -1)
-# np.savetxt(fit_res_filename, fit_result, fmt='%0.4f')
+test_set = Tooth13Dataset(root=data_dir, mode='test', transform=transform_test)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=False, num_workers=6)
 
-# print('All done ..')
+print('Start test ..')
+keypoints_estimate = test(net, test_loader, output_transform, device, dtype)
+
+test_res_filename = r'./test_res.txt'
+print('Write result to ', test_res_filename)
+# Reshape one sample keypoints in one line
+result = keypoints_estimate.reshape(keypoints_estimate.shape[0], -1)
+np.savetxt(test_res_filename, result, fmt='%0.4f')
+
+
+print('Start save fit ..')
+fit_set = Tooth13Dataset(root=data_dir, mode='train', transform=transform_test)
+fit_loader = torch.utils.data.DataLoader(fit_set, batch_size=1, shuffle=False, num_workers=6)
+keypoints_fit = test(net, fit_loader, output_transform, device=device, dtype=dtype)
+fit_res_filename = r'./fit_res.txt'
+print('Write fit result to ', fit_res_filename)
+fit_result = keypoints_fit.reshape(keypoints_fit.shape[0], -1)
+np.savetxt(fit_res_filename, fit_result, fmt='%0.4f')
+
+print('All done ..')
